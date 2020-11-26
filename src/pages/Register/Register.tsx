a449123ffa6891b151fa84ea22c6abe9de4415e5
TheCrether <denisimeri6@gmail.com>
2020-11-26 02:05:00 +0100

    :sparkles: make register page with input fields etc.
    styled it, but it needs the connection with the backend

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -7,7 +7,9 @@ import { OwnError } from "@models";
 import { putError } from "@store/app";
 
 import { remote } from "electron";
-import { Endpoints } from "@lib";
+import { Endpoints, validateEmail } from "@lib";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 
 const mapState = () => ({});
 
@@ -20,20 +22,156 @@ const connector = connect(mapState, mapDispatch);
 type PropsFromState = ConnectedProps<typeof connector>;
 type Props = PropsFromState;
 
-function RegisterUI({ putError }: Props) {
-  return (
-    <div id="register-wrapper">
-      <h1>Register</h1>
-      <div id="register-box"></div>
-      <div id="register-ticks">
-        <p>
-          I have read and agree to the <a href="..">Terms of Service</a>{" "}
-          agreement.
-        </p>
-        <input id="register-terms-tick" type="checkbox" />
+interface State {
+  email: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+  tos: boolean;
+  canRegister: boolean;
+}
+
+enum Change {
+  EMAIL,
+  USERNAME,
+  PASSWORD,
+  CONFIRMPASS,
+  TOS,
+}
+
+class RegisterUI extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
+      tos: false,
+      canRegister: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event: React.ChangeEvent<HTMLInputElement>, type: Change): void {
+    let upd: State = { ...this.state };
+    switch (type) {
+      case Change.USERNAME:
+        upd = { ...upd, username: event.target.value };
+        break;
+      case Change.EMAIL:
+        upd = { ...upd, email: event.target.value };
+        break;
+      case Change.PASSWORD:
+        upd = { ...upd, password: event.target.value };
+        break;
+      case Change.CONFIRMPASS:
+        upd = { ...upd, confirmPassword: event.target.value };
+        break;
+      case Change.TOS:
+        upd = { ...upd, tos: event.target.checked };
+        break;
+      default:
+        break;
+    }
+
+    if (
+      upd.username &&
+      validateEmail(upd.email) &&
+      upd.password.length >= 8 &&
+      upd.confirmPassword.length >= 8 &&
+      upd.password.normalize() === upd.confirmPassword.normalize() && // this is for graphemes like ê (but im not sure if we will include them in our password (probably not))
+      upd.tos
+    ) {
+      upd = { ...upd, canRegister: true };
+    } else {
+      upd = { ...upd, canRegister: false };
+    }
+
+    this.setState(upd);
+  }
+
+  openInBrowser() {
+    // TODO implement this
+    return;
+  }
+
+  render() {
+    const {
+      email,
+      username,
+      password,
+      confirmPassword,
+      tos,
+      canRegister,
+    } = this.state;
+
+    const { handleChange } = this;
+
+    return (
+      <div id="register-wrapper">
+        <h1>Register</h1>
+        <div id="register-box">
+          <label>
+            E-Mail:
+            <input
+              type="email"
+              placeholder="bob@example.com"
+              onChange={(e) => handleChange(e, Change.EMAIL)}
+              value={email}
+            />
+          </label>
+          <label>
+            Username:
+            <input
+              type="text"
+              placeholder="bob"
+              onChange={(e) => handleChange(e, Change.USERNAME)}
+              value={username}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              onChange={(e) => handleChange(e, Change.PASSWORD)}
+              value={password}
+            />
+          </label>
+          <label>
+            Confirm Password:
+            <input
+              type="password"
+              onChange={(e) => handleChange(e, Change.CONFIRMPASS)}
+              value={confirmPassword}
+            />
+          </label>
+          <label id="register-ticks">
+            <input
+              id="register-terms-tick"
+              type="checkbox"
+              checked={tos}
+              onChange={(e) => handleChange(e, Change.TOS)}
+            />
+            <p>
+              {/* TODO make terms of service */}I have read and agree to the{" "}
+              <a href="..">Terms of Service</a> agreement.
+            </p>
+          </label>
+          <input type="button" value="Register" disabled={!canRegister} />
+        </div>
+        <div className="login">
+          Already have an account? <a>Login</a>
+        </div>
+        <button id="browser">
+          <FontAwesomeIcon icon={faExternalLinkAlt} />
+          Open in Browser
+        </button>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export const Register = connector(RegisterUI);
