5d75457fa4138e6b38b874f7ee07cc089e38ea73
TheCrether <thecrether@gmail.com>
2020-12-06 20:45:51 +0100

    :sparkles added basic Welcome page

---
+++ src/pages/Welcome/Welcome.tsx
@@ -0,0 +1,34 @@
+import { DefaultDispatch } from "@store";
+import { push } from "connected-react-router";
+import React from "react";
+import { connect, ConnectedProps } from "react-redux";
+import "./Welcome.scss";
+
+const mapState = () => ({});
+const mapDispatch = (dispatch: DefaultDispatch) => ({
+  pushPath: (path: string) => dispatch(push(path)),
+});
+
+const connector = connect(mapState, mapDispatch);
+
+type PropsFromState = ConnectedProps<typeof connector>;
+type Props = PropsFromState;
+
+function WelcomeUI({ pushPath }: Props) {
+  return (
+    <div className="welcome-wrapper">
+      <div className="welcome-paths">
+        <button onClick={() => pushPath("/login")}>Login</button>
+        <button onClick={() => pushPath("/register")}>Register</button>
+      </div>
+      <button className="welcome-guest">
+        Continue as Guest
+        {/* TODO also make guest mode */}
+      </button>
+    </div>
+  );
+}
+
+export const Welcome = connector(WelcomeUI);
+
+export default Welcome;
