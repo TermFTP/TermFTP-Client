e7170c0751b3ea1351c9c1e415fa803eba365c4a
Adiber <adiber.yt@gmail.com>
2021-04-13 19:55:56 +0200

    :construction: added some LocalEndpoints

--- src/lib/LocalEndpoints.ts
+++ src/lib/LocalEndpoints.ts
@@ -1,10 +1,21 @@
 import { EditReq, GroupReq, GroupRes, GroupsRes, HistoryItemRes, HistoryReq, RemoveFromGroupReq, RemoveGroupReq, RemoveServerReq, SaveReq, SaveRes } from "@models";
 import { IRawParams } from "@shared/models";
+import * as idb from 'idb';
 
 export class LocalEndpoints implements IRawParams {
   [k: string]: any;
   
   private static _instance: LocalEndpoints;
+
+  private db: idb.IDBPDatabase;
+
+  private async getDatabase() {
+    this.db = await idb.openDB("termftp", 2, {
+      upgrade(db, oldVersion, newVersion, transaction) {
+        db.createObjectStore('server', {keyPath: 'id', autoIncrement: true});
+      }
+    })
+  }
 
   /**
    * gets the singleton instance for the Endpoints class
@@ -15,19 +26,56 @@ export class LocalEndpoints implements IRawParams {
     return this._instance;
   }
 
+  storeToIndexedDB = async (
+    store: string,
+    object = {}
+  ):Promise<string> => {
+
+    await this.getDatabase();
+
+    const tx = this.db.transaction(store, "readwrite");
+
+    const key: IDBValidKey = await tx.store.add(object);
+    await tx.done;
+
+    
+    console.log(key.toString())
+    return key.toString()
+  }
+
   historyItem = (req: HistoryReq): HistoryItemRes => {
     return null;
   };
 
-  saveServer = (req: SaveReq): SaveRes => {
-    return null;
+  saveServer = async (req: SaveReq): Promise<SaveRes> => {
+
+    return new Promise((resolve, reject) => {
+
+      this.storeToIndexedDB("server", req).then(id => {
+        resolve({
+          status: 200,
+          message: "Stored succesfully",
+          data: {
+            serverID: id,
+            ip: req.ip,
+            ftpPort: req.ftpPort,
+            lastConnection: req.lastConnection,
+            name: req.name,
+            password: req.password,
+            sshPort: req.sshPort,
+            username: req.username
+          }
+        });
+      });
+
+    });
   };
 
   editServer = (req: EditReq): SaveRes => {
     return null;
   };
 
-  fetchGroups = (): GroupsRes => {
+  fetchGroups = async(): Promise<GroupsRes> => {
     return null;
   };
 
