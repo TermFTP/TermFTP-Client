26f63acbed04b5839a51efb36abb6d9e98b56799
TheCrether <thecrether@gmail.com>
2021-11-23 18:47:18 +0100

    :children_crossing: fixed hotkey/focus issues after using searchbox also fixed focus issues for the pathbox and context menu fixed border radius context menu elements: - if the contextmenu is floating every border has a radius - if the contextmenu is at the bottom right, it seems like the plus and menu are connected fixed formatting in app/reducer added helper function for focusing the file manager

--- src/store/app/reducer.ts
+++ src/store/app/reducer.ts
@@ -3,63 +3,63 @@ import { Reducer } from "redux";
 import { BubbleModel } from "@models";
 
 export const initialState: AppState = {
-  data: {
-    isLoading: false,
-    bubbles: new Map<string, BubbleModel>(),
-  },
-  settingsOpen: false,
-  prompt: undefined,
-  autoLoggedIn: false
+	data: {
+		isLoading: false,
+		bubbles: new Map<string, BubbleModel>(),
+	},
+	settingsOpen: false,
+	prompt: undefined,
+	autoLoggedIn: false
 };
 
 export const appReducer: Reducer<AppState> = (state = initialState, action) => {
-  switch (action.type) {
-    case AppActionTypes.SET_LOADING: {
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          isLoading: action.payload,
-        },
-      };
-    }
-    case AppActionTypes.SET_PROMPT: {
-      return {
-        ...state,
-        prompt: action.payload,
-      };
-    }
-    case AppActionTypes.SET_SETTINGS: {
-      return {
-        ...state,
-        settingsOpen: action.payload,
-      };
-    }
-    case AppActionTypes.ADD_BUBBLE: {
-      if (state.data.bubbles.get(action.payload.key)) {
-        console.error(
-          `bubble with "${action.payload.key}" already exists, so it was not created`
-        );
-      }
-      const nMap = new Map<string, BubbleModel>(state.data.bubbles);
-      nMap.set(action.payload.key, action.payload.bubble);
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          bubbles: nMap,
-        },
-      };
-    }
-    case AppActionTypes.REMOVE_BUBBLE: {
-      const copy = new Map<string, BubbleModel>(state.data.bubbles);
-      copy.delete(action.payload.key);
-      return { ...state, data: { bubbles: copy } };
-      // return state;
-    }
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case AppActionTypes.SET_LOADING: {
+			return {
+				...state,
+				data: {
+					...state.data,
+					isLoading: action.payload,
+				},
+			};
+		}
+		case AppActionTypes.SET_PROMPT: {
+			return {
+				...state,
+				prompt: action.payload,
+			};
+		}
+		case AppActionTypes.SET_SETTINGS: {
+			return {
+				...state,
+				settingsOpen: action.payload,
+			};
+		}
+		case AppActionTypes.ADD_BUBBLE: {
+			if (state.data.bubbles.get(action.payload.key)) {
+				console.error(
+					`bubble with "${action.payload.key}" already exists, so it was not created`
+				);
+			}
+			const copy = new Map<string, BubbleModel>(state.data.bubbles);
+			copy.set(action.payload.key, action.payload.bubble);
+			return {
+				...state,
+				data: {
+					...state.data,
+					bubbles: copy,
+				},
+			};
+		}
+		case AppActionTypes.REMOVE_BUBBLE: {
+			const copy = new Map<string, BubbleModel>(state.data.bubbles);
+			copy.delete(action.payload.key);
+			return { ...state, data: { ...state.data, bubbles: copy } };
+			// return state;
+		}
+		default:
+			return state;
+	}
 };
 
 export default appReducer;
