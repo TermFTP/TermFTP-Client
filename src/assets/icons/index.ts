a33c982aab58ff2cd4c6a22c25a89f2fd7d8184e
TheCrether <thecrether@gmail.com>
2021-12-07 15:34:12 +0100

    :zap: improve icon loading by replacing SVGs with PNGs
    react automatically converts images <10kB to inline base64

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,44 +1,44 @@
-import css from "./CSS.svg";
-import docx from "./DOCX.svg";
-import empty from "./EMPTY.svg";
-import exe from "./EXE.svg";
-import folder from "./FOLDER.svg";
-import html from "./HTML.svg";
-import image from "./IMAGE.svg";
-import java from "./JAVA.svg";
-import js from "./JS.svg";
-import json from "./JSON.svg";
-import log from "./LOG.svg";
-import md from "./MARKDOWN.svg";
-import music from "./MUSIC.svg";
-import pdf from "./PDF.svg";
-import pptx from "./PPTX.svg";
-import shell from "./SHELL.svg";
-import sql from "./SQL.svg";
-import txt from "./TXT.svg";
-import video from "./VIDEO.svg";
-import xlsx from "./XLSX.svg";
-import zip from "./ZIP.svg";
+import css from "./CSS.png";
+import docx from "./DOCX.png";
+import empty from "./EMPTY.png";
+import exe from "./EXE.png";
+import folder from "./FOLDER.png";
+import html from "./HTML.png";
+import image from "./IMAGE.png";
+import java from "./JAVA.png";
+import js from "./JS.png";
+import json from "./JSON.png";
+import log from "./LOG.png";
+import md from "./MARKDOWN.png";
+import music from "./MUSIC.png";
+import pdf from "./PDF.png";
+import pptx from "./PPTX.png";
+import shell from "./SHELL.png";
+import sql from "./SQL.png";
+import txt from "./TXT.png";
+import video from "./VIDEO.png";
+import xlsx from "./XLSX.png";
+import zip from "./ZIP.png";
 export default {
-  css,
-  docx,
-  empty,
-  exe,
-  folder,
-  html,
-  image,
-  java,
-  js,
-  json,
-  log,
-  md,
-  music,
-  pdf,
-  pptx,
-  shell,
-  sql,
-  txt,
-  video,
-  xlsx,
-  zip
+	css,
+	docx,
+	empty,
+	exe,
+	folder,
+	html,
+	image,
+	java,
+	js,
+	json,
+	log,
+	md,
+	music,
+	pdf,
+	pptx,
+	shell,
+	sql,
+	txt,
+	video,
+	xlsx,
+	zip
 };
