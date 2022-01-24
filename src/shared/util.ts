c58b74b6461fa12b8811ecdc940888fe217f4aba
TheCrether <denisimeri6@gmail.com>
2022-01-24 20:45:58 +0100

    :sparkles: fix getIsDev so built TermFTP works again

--- src/shared/util.ts
+++ src/shared/util.ts
@@ -1,9 +1,12 @@
 export const isDev = getIsDev();
 
 function getIsDev(): boolean {
-	const ENV = process.env.NODE_ENV || window?.process.env.NODE_ENV;
+	let win: Window & typeof globalThis = undefined;
+	if (typeof window === "undefined") win = {} as Window & typeof globalThis
+	else win = window
+	const ENV = process?.env?.NODE_ENV || win?.process?.env?.NODE_ENV;
 	if (ENV) {
-		return ENV.toLowerCase().includes("dev");
+		return ENV?.toLowerCase().includes("dev");
 	} else {
 		return false
 	}
