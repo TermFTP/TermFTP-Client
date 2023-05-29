43f172c01ee92ffd3b934ebfedbad1f51172c05c
TheCrether <thecrether@gmail.com>
2023-05-29 21:47:03 +0200

    :fire: problems with keytar when running on other machines remove unnecessary files

--- tools/webpack/webpack.main.js
+++ tools/webpack/webpack.main.js
@@ -3,14 +3,14 @@ module.exports = {
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
-  entry: ['./src/main/app.ts'],
+  entry: ["./src/main/app.ts"],
   // Put your normal webpack config below here
   module: {
-    rules: require('./webpack.rules'),
+    rules: require("./webpack.rules"),
   },
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
-    alias: require('./webpack.aliases'),
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
+    alias: require("./webpack.aliases"),
   },
-  stats: 'minimal',
+  stats: "minimal",
 };
